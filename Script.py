829b33929758976e8b3329d555b799d42deb2b7c
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-04-13 14:06:02 -0500

    Actualizar Script.py

--- Script.py
+++ Script.py
@@ -145,17 +145,14 @@ Nᴏᴍʙʀᴇ - {}</b>
 
 
     # --- MENSAJE PARA FORZAR SUSCRIPCIÓN ---
-    FORCE_MSG = """<blockquote><b> Acceso Restringido ⚠️</b>
+    FORCE_MSG = """<b>⚠️ Acceso Restringido ⚠️</b>
 
 <b>Hola {mention} 👋!
-Debes ser miembro del CANAL
-para acceder a estos archivos.
+Debes ser miembro del CANAL para acceder a estos archivos.
 
-📣 Una vez que te hayas unido,
-por favor, presiona el botón 
-'Intentar de Nuevo'.
+📣 Una vez que te hayas unido, por favor, presiona el botón 'Intentar de Nuevo'.
 
-¡Gracias por tu apoyo! 😊</b></blockquote>"""
+¡Gracias por tu apoyo! 😊</b>"""
 
     # ... (resto de la clase si hay más) ...
 
